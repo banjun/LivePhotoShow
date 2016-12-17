4d64cf963a96d83aa681a3676ce77684e499f341
banjun <banjun@gmail.com>
2016-12-17 14:17:05 +0900

    play first matched livephoto

--- LivePhotoShow/AppDelegate.swift
+++ LivePhotoShow/AppDelegate.swift
@@ -2,8 +2,9 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    let window = UIWindow(frame: UIScreen.main.bounds)
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        let window = UIWindow(frame: UIScreen.main.bounds)
         window.rootViewController = UINavigationController(rootViewController: ViewController(nibName: nil, bundle: nil))
         window.makeKeyAndVisible()
         return true
--- LivePhotoShow/ViewController.swift
+++ LivePhotoShow/ViewController.swift
@@ -1,19 +1,52 @@
 import UIKit
 import Photos
+import PhotosUI
+import NorthLayout
+import Ikemen
 
 class ViewController: UIViewController {
+    let countTitleLabel = UILabel() ※ { l in
+        l.text = "Live Photos in Photo Library"
+        l.textAlignment = .center
+    }
+    let countLabel = UILabel() ※ { l in
+        l.text = "(fetching...)"
+        l.textAlignment = .center
+    }
+    private lazy var livePhotoView: PHLivePhotoView = PHLivePhotoView()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        title = "LivePhotoShow"
         view.backgroundColor = .white
 
+        let autolayout = northLayoutFormat([:], [
+            "countTitle": countTitleLabel,
+            "count": countLabel,
+            "livephoto": livePhotoView])
+        autolayout("H:|[countTitle]|")
+        autolayout("H:|[count]|")
+        autolayout("H:|[livephoto]|")
+        autolayout("V:|-[countTitle]-[count]-[livephoto]|")
+    }
 
-        let lib = PHPhotoLibrary.shared()
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
 
-        var options = PHFetchOptions()
-        options.predicate = NSPredicate(format: "(mediaSubtype & %d) > 0", PHAssetMediaSubtype.photoLive.rawValue)
+        let options = PHFetchOptions()
+        options.predicate = NSPredicate(format: "(mediaSubtype & %d) > 0 && pixelHeight == 1334", PHAssetMediaSubtype.photoLive.rawValue)
         let result = PHAsset.fetchAssets(with: .image, options: options)
         NSLog("%@", "count = \(result.count)")
+        countLabel.text = "\(result.count) live photos"
+
+        guard let asset = result.firstObject else { return }
+        PHImageManager.default().requestLivePhoto(for: asset, targetSize: view.bounds.size, contentMode: .aspectFill, options: nil) { photo, d in
+            DispatchQueue.main.async {
+                self.livePhotoView.livePhoto = photo
+                self.livePhotoView.startPlayback(with: .full)
+            }
+        }
     }
 }
 
